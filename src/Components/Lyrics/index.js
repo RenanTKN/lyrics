15605afee88a00737dc8f3b7c384596c6bab66c6
RenanTKN <renanwds@gmail.com>
2020-03-12 20:41:21 -0300

    Add code

---
+++ src/Components/Lyrics/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default ({
+    band,
+    song,
+    loading,
+    lyrics
+}) =>
+    lyrics ?
+    <div className="lyrics-container">
+        <div className="lyrics justify-content-center">
+            <pre>
+                {lyrics !== "No lyrics found" &&
+                    <>
+                        <h5>{band} - {song}</h5>
+                        <hr/>
+                    </>
+                }
+                {lyrics}
+            </pre>
+        </div>
+    </div>
+    : loading ?
+    <div className="spinner mt-3">
+        {loading}
+    </div>
+    : null
