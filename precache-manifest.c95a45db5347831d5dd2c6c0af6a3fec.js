10ca127170b0c1871406b67aeb38a27aba303a4d
RenanTKN <renanwds@gmail.com>
2020-04-08 15:23:33 -0300

    Updates

---
+++ precache-manifest.c95a45db5347831d5dd2c6c0af6a3fec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c81486887c832ba7ae4a3e287f9797",
+    "url": "/lyrics/index.html"
+  },
+  {
+    "revision": "1bdfd4b31aba045857a7",
+    "url": "/lyrics/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4f6133090248b66436a4",
+    "url": "/lyrics/static/css/main.2fe808ea.chunk.css"
+  },
+  {
+    "revision": "1bdfd4b31aba045857a7",
+    "url": "/lyrics/static/js/2.48329816.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/lyrics/static/js/2.48329816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f6133090248b66436a4",
+    "url": "/lyrics/static/js/main.04afd590.chunk.js"
+  },
+  {
+    "revision": "a0c89214e3aaeb666338",
+    "url": "/lyrics/static/js/runtime-main.c747770d.js"
+  },
+  {
+    "revision": "d78d2d75e0eaf58fe75226b8cf2c3345",
+    "url": "/lyrics/static/media/bg.d78d2d75.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/lyrics/precache-manifest.c95a45db5347831d5dd2c6c0af6a3fec.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lyrics/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
